6fd2bf203ba44fad447df70db7526b868d0798e5
Nikolay Kyrylishyn <53525185+kyrylishynnick@users.noreply.github.com>
2024-05-16 12:07:48 +0300

    Add files via upload

---
+++ HW_4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написати програму, яка обчислює середнє арифметичне двох чисел.
+
+Console.WriteLine("Hello, please enter the first number");
+double firstNumber = double.Parse(Console.ReadLine());
+Console.WriteLine("Hello, please enter the second number");
+double secondNumber = double.Parse(Console.ReadLine());
+double avarageNumber = (firstNumber + secondNumber) / 2;
+
+Console.WriteLine($"The avarage number is: {avarageNumber}");
