d22dca6dc9b976c1e39793df1bc17ba4e2976d3c
Nikolay Kyrylishyn <53525185+kyrylishynnick@users.noreply.github.com>
2024-05-16 12:06:38 +0300

    Add files via upload

---
+++ HW_2/Program.cs
@@ -0,0 +1,7 @@
+﻿//Написати програму, яка обчислює квадрат будь-якого введеного числа (бажано використати Math).
+
+Console.WriteLine("Inpur please a number:");
+double userInput = double.Parse(Console.ReadLine());
+
+double square = Math.Pow(userInput, 2);
+Console.WriteLine($"The square of the number {userInput} is {square}");
