b3097687bc1b6881a7ca937f4681a3c0323dcf46
Nikolay Kyrylishyn <53525185+kyrylishynnick@users.noreply.github.com>
2024-05-16 12:05:57 +0300

    Add files via upload

---
+++ HW_1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Дано тризначне число. Знайти число, отримане під час прочитання його цифр справа наліво.
+
+int number = 123;
+int symbol1;
+int symbol2;
+int symbol3;
+int firstTemporaryDig;
+int secondTemporaryDig;
+
+
+
+symbol1 = number % 10;
+firstTemporaryDig = number / 10;
+symbol2 = firstTemporaryDig % 10;
+secondTemporaryDig = number / 10;
+symbol3 = firstTemporaryDig / 10;
+
+
+int reversedNumber = symbol1 * 100 + symbol2 * 10 + symbol3;
+
+Console.WriteLine($"The reverce number is: {reversedNumber}");
