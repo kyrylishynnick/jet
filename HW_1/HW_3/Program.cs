a4a385fd7c8495738127d41f9c948d577dad6269
Nikolay Kyrylishyn <53525185+kyrylishynnick@users.noreply.github.com>
2024-05-16 12:10:42 +0300

    Add files via upload

---
+++ HW_1/HW_3/Program.cs
@@ -0,0 +1,16 @@
+﻿//Дано тризначне число. Знайти число, отримане під час перестановки першої та другої цифр заданого числа.
+
+int number = 246;
+
+int firstTemporaryDig;
+int secondTemporaryDig;
+int thirtdTemporaryDig;
+
+
+firstTemporaryDig = number / 100;
+secondTemporaryDig = number / 10 % 10;
+thirtdTemporaryDig = number % 10;
+
+int revercedNumber = secondTemporaryDig * 100 + firstTemporaryDig * 10 + thirtdTemporaryDig;
+
+Console.WriteLine($"The reverce number is: {revercedNumber}");
